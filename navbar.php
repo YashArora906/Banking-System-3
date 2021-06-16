84689f18b62955d3c08561d21a0e84346e474bcc
YashArora906 <85866499+YashArora906@users.noreply.github.com>
2021-06-16 09:24:16 +0530

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,23 @@
+<!-- navbar --> 
+<nav class="navbar navbar-expand-md navbar-light bg-light">
+<a class="navbar-brand" href="index.php"><b>SPARKS FOUNDATION</b></a>
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+<span class="navbar-toggler-icon"></span>
+</button>
+<div class="collapse navbar-collapse" id="collapsibleNavbar">
+<ul class="navbar-nav ml-auto">
+<li class="nav-item">
+<a class="nav-link" href="index.php" style="color: black;" >HOME</a>
+</li>
+
+<li class="nav-item">
+<a class="nav-link" href="viewallcustomers.php" style="color: black;">VIEW ALL USERS</a>
+</li>
+<li class="nav-item">
+<a class="nav-link" href="transfermoney.php" style="color: black;">TRANSFER MONEY</a>
+</li>
+<li class="nav-item">
+<a class="nav-link" href="transactionhistory.php" style="color: black;">TRANSACTION HISTORY</a>
+</li>
+</div>
+</nav>
